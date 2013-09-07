457eac85e552f5a46cf9dd6ef187e1e4b3ee756e
Matt Buck <github@matthewbuck.com>
2013-09-07 18:55:29 -0500

    Framework for pizza order system.

---
+++ pizza.rb
@@ -0,0 +1,9 @@
+module Pizza
+  class Pie
+
+  end
+
+  class Topping
+
+  end
+end
---
+++ spec/pizza_spec.rb
@@ -0,0 +1,12 @@
+require 'rubygems'
+require 'bundler/setup'
+require 'rspec'
+require_relative '../pizza'
+
+describe Pizza::Pie do
+
+end 
+
+describe Pizza::Topping do
+
+end
